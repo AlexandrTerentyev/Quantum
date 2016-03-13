990f71c1c3d34b7385a3612b98898cedcafb9660
Alexandr Terentyev <terentyev@gradoservice.ru>
2016-03-13 19:54:09 +0300

    Load/Save operations. Qubits checking before transformations. Destroy qubits after measure

--- src/kpfu/terentyev/quantum_api/QuantumMemoryManager.java
+++ src/kpfu/terentyev/quantum_api/QuantumMemoryManager.java
@@ -5,13 +5,58 @@ import kpfu.terentyev.quantum_emulator.OneStepAlgorythm;
 import kpfu.terentyev.quantum_emulator.Gates.PhaseGate;
 import kpfu.terentyev.quantum_emulator.OneStepTwoQubitControlledGateAlgorythm;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by aleksandrterentev on 08.03.16.
  */
 public class QuantumMemoryManager extends QuantumManager {
 
+    private int proccessorsUnitsCount = 4;
+    private static final String qubitDestroyedRegisterAddress = "Qubit destroyed";
+
+
+    Set proccessorsUnitsRegisters;
+
+    public QuantumMemoryManager(){
+        proccessorsUnitsRegisters = new HashSet<Qubit>();
+    }
+
 //    Base operations
+
+    public boolean qubitIsAlreadyInProccessor (Qubit q){
+        return proccessorsUnitsRegisters.contains(q);
+    }
+
+    public void load (Qubit q) throws Exception {
+        if (proccessorsUnitsRegisters.size() == proccessorsUnitsCount && !qubitIsAlreadyInProccessor(q)){
+            throw (new Exception("Processor units overflow"));
+        }
+
+        proccessorsUnitsRegisters.add(q);
+    }
+
+    public void save (Qubit q){
+        proccessorsUnitsRegisters.remove(q);
+    }
+
+
+    void checkQubitsBeforePerformTransformation (Qubit ... qubits) throws Exception {
+        for (Qubit q: qubits) {
+            if (q.registerAddress.equals(qubitDestroyedRegisterAddress)) {
+                throw (new Exception("One ore more qubits already destroyed!"));
+            } else if (!qubitIsAlreadyInProccessor(q)) {
+                throw (new Exception("Operating qubits are not loaded to proccessor units!"));
+            }
+        }
+    }
+
+    /**
+     * This tranformation must be performed for qubit that loaded to proccessor unit
+     * */
     void phase (double thetaInRadians, Qubit qubit) throws Exception {
+        checkQubitsBeforePerformTransformation(qubit);
         RegisterInfo registerInfo = registers.get(qubit.registerAddress);
         OneStepAlgorythm oneStepAlgorythm = new OneStepAlgorythm(registerInfo.register.getQubitsNumber(),
                 new PhaseGate(thetaInRadians),
@@ -20,7 +65,11 @@ public class QuantumMemoryManager extends QuantumManager {
         registerInfo.register.performAlgorythm(oneStepAlgorythm);
     }
 
+    /**
+     * This tranformation must be performed for qubit that loaded to proccessor unit
+     * */
     void QET (double thetaInRadians, Qubit qubit) throws Exception {
+        checkQubitsBeforePerformTransformation(qubit);
         RegisterInfo registerInfo = registers.get(qubit.registerAddress);
         Complex[][] matrix = {
                 {new Complex(Math.cos(thetaInRadians/2), 0), new Complex(0, Math.sin(thetaInRadians/2))},
@@ -33,7 +82,11 @@ public class QuantumMemoryManager extends QuantumManager {
         registerInfo.register.performAlgorythm(oneStepAlgorythm);
     }
 
+    /**
+     * This tranformation must be performed for qubits that loaded to proccessor units
+     * */
     void cQET (double thetaInRadians, Qubit controllingQubit, Qubit controlledQubit) throws Exception {
+        checkQubitsBeforePerformTransformation(controlledQubit, controllingQubit);
         RegisterInfo registerInfo = checkAndMergeRegistersIfNeedForQubits(controllingQubit, controlledQubit);
 //        TODO: fill matrix!!!
         Complex[][] matrix = {};
@@ -47,6 +100,10 @@ public class QuantumMemoryManager extends QuantumManager {
     }
 
     int measure (Qubit qubit) throws Exception {
-        return registers.get(qubit.registerAddress).register.measureQubit(qubit.addressInRegister);
+        RegisterInfo regInfo = registers.get(qubit.registerAddress);
+        int result = regInfo.register.measureQubit(qubit.addressInRegister);
+        regInfo.qubits.remove(qubit.addressInRegister);
+        qubit.registerAddress = qubitDestroyedRegisterAddress;
+        return result;
     }
 }
